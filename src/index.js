6cea3d5f069665dfb15cc1e6d421488371582804
loakantse <loakantse.malewa@gmail.com>
2025-02-23 12:01:48 +0200

    changed html to sync with api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+function updateWeather(response) {
+let temperatureElement = document.querySelector("#degree")
+let temperature = response.data.temperature.current
+temperatureElement.innerHTML = Math.round(temperature)
+}
+
 function searchCity(city) {
 let apiKey ="ebeacdf310b43e22a85a46d12ot9f543"
+let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric'
+axios.get(apiUrl).then(updateWeather)
 }
 
 function handleSearch(event) {
