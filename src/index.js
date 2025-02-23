599c3caa7f07b71eae08c5019a31f86adecb6f88
loakantse <loakantse.malewa@gmail.com>
2025-02-23 17:30:51 +0200

    updated information about current weather

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 function updateWeather(response) {
 let temperatureElement = document.querySelector("#degree")
 let temperature = response.data.temperature.current
+let cityElement = document.querySelector("#city")
+let descriptionElement = document.querySelector("#description")
+
 temperatureElement.innerHTML = Math.round(temperature)
+cityElement.innerHTML = response.data.city
+descriptionElement.innerHTML = response.data.condition.description
 }
 
 function searchCity(city) {
-let apiKey ="ebeacdf310b43e22a85a46d12ot9f543"
-let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric'
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=b2a5adcct04b33178913oc335f405433&units=metric"
 axios.get(apiUrl).then(updateWeather)
 }
 
 function handleSearch(event) {
     event.preventDefault()
     let searchInput = document.querySelector("#search-input-form")
-let cityElement = document.querySelector("#city")
-cityElement.innerHTML = searchInput.value
+
 searchCity(searchInput.value)
 }
 
