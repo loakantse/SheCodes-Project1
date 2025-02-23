ca616fdf7367ff8145d6c5f50e932c4ed00e17cc
loakantse <loakantse.malewa@gmail.com>
2025-02-23 11:33:07 +0200

    added javascript to the file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearch(event) {
+    event.preventDefault()
+    let searchInput = document.querySelector("#search-input-form")
+let cityElement = document.querySelector("#city")
+cityElement.innerHTML = searchInput.value
+}
+
+let searchFormElement = document.querySelector("#search-form")
+searchFormElement.addEventListener("submit",handleSubmit)
