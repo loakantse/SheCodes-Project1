3799ab321518cd7ab39334323f1e8ebe3affebb5
loakantse <loakantse.malewa@gmail.com>
2025-02-23 11:46:33 +0200

    created new function called searchCity and added api key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+function searchCity(city) {
+let apiKey ="ebeacdf310b43e22a85a46d12ot9f543"
+}
+
 function handleSearch(event) {
     event.preventDefault()
     let searchInput = document.querySelector("#search-input-form")
 let cityElement = document.querySelector("#city")
 cityElement.innerHTML = searchInput.value
+searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form")
